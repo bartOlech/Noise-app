d173dc99df4e1047803550c2f114d72663aeabaa
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-11-18 00:04:13 +0100

    correct the buttons if it is pressed

--- src/containers/mainContent.js
+++ src/containers/mainContent.js
@@ -6,35 +6,50 @@ import PlayIcon from '../img/play_icon.png'
 const Content = styled.div`
        
     `;
+const btnStyle = {
+    animation: {animation: 'clicked 150ms'},
+    checked: {background: 'black'}
+};
+const visibleImg = {
+    opacity: '.85',
+    fontSize: '16px',
+    width: '109%',
+    height: '109%',
+    transitionProperty: 'none',
+    left: '-4px',
+    top: '-4px'
+};
+
+let animationValue = '';
 
 class Header extends Component{
-    state = {
-        babySoundBtn: 'Dziecko',
-        adultSoundBtn: 'Dorosły',
-        rainSoundBtn: 'Deszcz',
-        isClicked: false
+    constructor(props){
+        super(props)
+        this.state= {
+            babySoundBtn: 'Dziecko',
+            adultSoundBtn: 'Dorosły',
+            rainSoundBtn: 'Deszcz',
+            isClicked: false,
+        }
     }
 
-    playBtn = () =>{
-        this.setState({
-            isClicked: true
-        })
-        setTimeout(()=>{
+        clickBtn = (val)=>{
             this.setState({
-                isClicked: false
+                isClicked: true
             })
-        }, 500)
-    }
+            animationValue = val
+        }
     
     render(){
+        const {isClicked, babySoundBtn, adultSoundBtn, rainSoundBtn} = this.state;
         return(
             <Content>
               <ul className='music-btn-list'>
-                    <li><div onClick={()=>this.playBtn()} class={this.state.isClicked?'round baby clicked-btn':'round baby'}>{this.state.babySoundBtn}<div class="round"><div className='btn-ctn'><img src={PlayIcon} alt='play ico'></img></div></div></div>
+                    <li><button onClick={() => this.clickBtn('baby')} onAnimationEnd={() => this.setState({isClicked: false})} style={isClicked && animationValue === 'baby'?btnStyle.animation:null} className={animationValue === 'baby'?' checked-btn-button round baby':'round baby'}>{babySoundBtn}<div style={animationValue === 'baby'?visibleImg:null} className='round'><img src={PlayIcon} alt='play ico'></img></div></button>
                     </li>
-                    <li><div onClick={()=>this.playBtn()} class={this.state.isClicked?'round adult clicked-btn':'round adult'} >{this.state.adultSoundBtn}<div class="round"><img src={PlayIcon} alt='play ico'></img></div></div>
+                    <li><button onClick={() => this.clickBtn('adult')} onAnimationEnd={() => this.setState({isClicked: false})} style={isClicked && animationValue === 'adult'?btnStyle.animation:null} className={animationValue === 'adult'?' checked-btn-button round adult':'round adult'}>{adultSoundBtn}<div style={animationValue === 'adult'?visibleImg:null} className="round"><img src={PlayIcon} alt='play ico'></img></div></button>
                     </li>
-                    <li><div onClick={()=>this.playBtn()} class={this.state.isClicked?'round rain clicked-btn':'round rain'}>{this.state.rainSoundBtn}<div class="round"><img src={PlayIcon} alt='play ico'></img></div></div>
+                    <li><button onClick={() => this.clickBtn('rain')} onAnimationEnd={() => this.setState({isClicked: false})} style={isClicked && animationValue === 'rain'?btnStyle.animation:null} class={animationValue === 'rain'?' checked-btn-button round rain':'round rain'}>{rainSoundBtn}<div style={animationValue === 'rain'?visibleImg:null} className="round"><img src={PlayIcon} alt='play ico'></img></div></button>
                     </li>
                  </ul> 
             </Content>
