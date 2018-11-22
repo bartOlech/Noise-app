476cc96d4077ad8ad7d0e9fa903f294ee9ceffb1
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-11-22 13:22:35 +0100

    add animation to the category buttons

--- src/containers/category.js
+++ src/containers/category.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import '../App.css';
+import styled, { keyframes } from 'styled-components';
+import { bounceInLeft, fadeOutRight } from 'react-animations';
 
-const hideCtn = {
-    display:'none'
-}
+const bounceAnimation = keyframes`${bounceInLeft}`;
+const fadeOutRightAnimation = keyframes`${fadeOutRight}`;
+ 
+const BouncyDiv = styled.div`
+  animation: 700ms forwards ${props =>props.animationVal};
+`;
 class Category extends Component{
     constructor(props){
         super(props)
@@ -19,18 +24,21 @@ class Category extends Component{
     }
 
     render(){
+        const {visibleContent} = this.state;
         return(
-            <div style={this.state.visibleContent?null:hideCtn} className='category-content'>
-                <h3>Wybierz kategorię:</h3>
-                <div className='content-buttons'>
-                    <button className='ctg-btn ctg-btn-1' onClick={()=>this.chooseCategory('learn')}><span className='ctg-text'>Nauka</span>
-                    </button>
-                    <button className='ctg-btn ctg-btn-2' onClick={()=>this.chooseCategory('relax')}><span className='ctg-text'>Relaks</span>
-                    </button>
-                    <button className='ctg-btn ctg-btn-3' onClick={()=>this.chooseCategory('sleep')}><span className='ctg-text'>Sen</span>
-                    </button>
+            <BouncyDiv animationVal={visibleContent?bounceAnimation:fadeOutRightAnimation}>
+                <div className='category-content'>
+                    <h3>Wybierz kategorię:</h3>
+                    <div className='content-buttons'>
+                        <button className='ctg-btn ctg-btn-1' onClick={()=>this.chooseCategory('learn')}><span className='ctg-text'>Nauka</span>
+                        </button>
+                        <button className='ctg-btn ctg-btn-2' onClick={()=>this.chooseCategory('relax')}><span className='ctg-text'>Relaks</span>
+                        </button>
+                        <button className='ctg-btn ctg-btn-3' onClick={()=>this.chooseCategory('sleep')}><span className='ctg-text'>Sen</span>
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </BouncyDiv>
         )
     }
 }
