df1715efc643f18beb454e3a4c353d20970b1134
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-11-30 14:32:56 +0100

    create an audio player

--- src/containers/mainContent.js
+++ src/containers/mainContent.js
@@ -4,6 +4,8 @@ import '../App.css';
 import PlayIcon from '../img/play_icon.png';
 import Category from './category';
 import { zoomIn } from 'react-animations';
+import bellSound1 from '../sounds/bell.mp3'
+import bellSound2 from '../sounds/bell2.mp3'
 
 const fadeInLeftAnimation = keyframes`${zoomIn}`;
 
@@ -28,26 +30,38 @@ class Header extends Component{
     constructor(props){
         super(props)
         this.state= {
-            button1: 'Dziecko',
-            button2: 'Dorosły',
-            button3: 'Deszcz',
+            button1: '',
+            button2: '',
+            button3: '',
             isClicked: false,
             category: '',
             animationValue: '',
+            currentSelectedCtg: '',
+            btn1: false,
+            btn2: false,
+            btn3: false,
+            curSoundBtnClicked: ''
         }
         this.child = React.createRef();
     }
 
         clickBtn = (val)=>{
+
             this.setState({
                 isClicked: true,
                 animationValue: val,
+                currentSelectedCtg: val
             })
             if(val === this.state.animationValue){
                 this.setState({
-                    animationValue: ''
+                    animationValue: '',
+                    currentSelectedCtg: ''
                 })
-            }
+            }  
+            val === 'btn1'?this.setState({btn1: true}):this.setState({btn1: false});
+            val === 'btn2'?this.setState({btn2: true}):this.setState({btn2: false});
+            val === 'btn3'?this.setState({btn3: true}):this.setState({btn3: false});
+
         }
         setValue = (val)=>{
             setTimeout(()=>{
@@ -84,12 +98,36 @@ class Header extends Component{
             this.child.current.resaveViews();
           }
 
+          playSound(){
+
+            const {currentSelectedCtg, btn1, btn2, btn3, category,} = this.state;
+            let currentSound = null;
+            let curSoundBtnClicked = null;
+            if(category === 'learn'){
+               if(btn1){currentSound = bellSound1; curSoundBtnClicked = 'btn1'}else
+               if(btn2){currentSound = bellSound2; curSoundBtnClicked = 'btn2'}else
+               if(btn3){currentSound = bellSound1; curSoundBtnClicked = 'btn3'}
+            }else if(category === 'relax'){
+                
+            }else if(category === 'sleep'){
+
+            }
+              return(
+                <audio src={currentSound} muted={curSoundBtnClicked !== currentSelectedCtg?true:false} loop autoPlay/>
+              )
+          }
+
     render(){
         const {isClicked, button1, button2, button3, category, animationValue} = this.state;
         return(
         <div className='main-content'>
-            <Category ref={this.child} ctgValue={this.setValue}></Category>
+         
+            <Category  ref={this.child} ctgValue={this.setValue}></Category>
             <Content displayValue={category?'flex':'none'}>
+
+           {/* audio */}
+                {this.playSound()}
+            
               <ul className='music-btn-list'>
                     <li><button onClick={() => this.clickBtn('btn1')} onAnimationEnd={() => this.setState({isClicked: false})} style={isClicked && animationValue === 'btn1'?btnStyle.animation:null} className={animationValue === 'btn1'?' checked-btn-button round btn1':'round btn1'}>{button1}<div style={animationValue === 'btn1'?visibleImg:null} className='round'><img src={PlayIcon} alt='play ico'></img></div></button>
                     </li>
