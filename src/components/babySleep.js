a7725a5d0a75be93b57dc6af0429a6714a12b8df
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-12-08 17:26:10 +0100

    add a button to the BabySleep component

--- src/components/babySleep.js
+++ src/components/babySleep.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import babySleepImg from '../img/babyImg.png';
+import babySleepImg from '../img/sleep.png';
 
 const HideStyle = {
     display: 'none'
 }
 const ImgStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    position: 'absolute',
-    left: '10%'
+    width: '100px',
+    height: '100px'
 
 }
 
@@ -38,7 +36,32 @@ class BabySleep extends Component{
         const {imgIsVisible, isVisible} = this.state;
         return(
             <div style={isVisible?null:HideStyle}>
-                <img style={imgIsVisible?ImgStyle:HideStyle} src={babySleepImg}></img>
+                <div className='babySleepCnt'>
+                    <h3 className='babySleepText'>Uśpij dziecko</h3>
+                    <img style={imgIsVisible?ImgStyle:HideStyle} src={babySleepImg}></img>
+                </div>
+                <div className='play-btn-section'>
+                <div class="normal-container">
+                    <div class="smile-rating-container">
+                        <div class="smile-rating-toggle-container">
+                            <form class="submit-rating">
+                                <input id="meh"  name="satisfaction" type="radio" /> 
+                                <input id="fun" name="satisfaction" type="radio" /> 
+                                <label for="meh" class="rating-label rating-label-meh">Off</label>
+                                <div class="smile-rating-toggle"></div>
+                                
+                                <div class="rating-eye rating-eye-left"></div>
+                                <div class="rating-eye rating-eye-right"></div>
+                                
+                                <div class="mouth rating-eye-bad-mouth"></div>
+                                
+                                <div class="toggle-rating-pill"></div>
+                                <label for="fun" class="rating-label rating-label-fun">On</label>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
             </div>
         )
     }
